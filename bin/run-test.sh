631b3a3a5a4b737277f481285e3ec3e781ed3c9a
Tetsuro Yoshikawa <phper.0o0@gmail.com>
2023-08-07 22:57:24 +0900

    chore: allow all arguments.

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-RUN_TEST_COMMAND="jest --config jest.config.js ${1}"
+RUN_TEST_COMMAND="jest --config jest.config.js ${@}"
 firebase emulators:exec --only firestore "$RUN_TEST_COMMAND"
